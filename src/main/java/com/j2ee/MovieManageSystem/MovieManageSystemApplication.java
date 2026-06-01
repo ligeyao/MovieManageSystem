0137c8c8f330e6c80791a1d18c71ff1e6abb8b39
ligeyao <3071579530@qq.com>
2026-06-01 14:42:30 +0800

    initialization

---
+++ src/main/java/com/j2ee/MovieManageSystem/MovieManageSystemApplication.java
@@ -0,0 +1,13 @@
+package com.j2ee.MovieManageSystem;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MovieManageSystemApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MovieManageSystemApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/j2ee/MovieManageSystem/MovieManageSystemApplicationTests.java
@@ -0,0 +1,13 @@
+package com.j2ee.MovieManageSystem;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MovieManageSystemApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
